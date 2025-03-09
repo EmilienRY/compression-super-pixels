75fac3c1adf1a6cc05dc30d8c810e9f98c2889fa
Juan José Parra <jjparrad@gmail.com>
2025-03-09 13:26:03 +0100

    Code cleaning and cluster search area adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,11 @@
 #include "Vec4.h"
 
 const int COMPACTNESS = 10; // m
-std::map<int, Vec3> clusterColors;
 
-void averageClusterColor(ImageBase& imIn, Vec2 center) {
+std::vector<Vec3> clusterColors;
+std::vector<Vec2> clusterCenters;
+
+Vec3 averageClusterColor(ImageBase& imIn, Vec2 center) {
   // TODO: Naive approach. Must be changed
   ImageBase imR = *imIn.getPlan(ImageBase::PLAN_R);
   ImageBase imG = *imIn.getPlan(ImageBase::PLAN_G);
@@ -42,23 +44,19 @@ void averageClusterColor(ImageBase& imIn, Vec2 center) {
   }
 
   Vec3 color(sumR/countR, sumG/countG, sumB/countB);
-
-  int centerId = ((x + y) * (x + y + 1)) / 2 + y;
-  clusterColors[centerId] = color;
+  return color;
 }
 
-std::vector<Vec2> findInitialCenters(ImageBase& imIn, int S) {
-  std::vector<Vec2> centers;
+void findInitialCenters(ImageBase& imIn, int S) {
   for (int y = 0; y < imIn.getHeight(); ++y) {
     for (int x = 0; x < imIn.getWidth(); ++x) {
       if (x % S == S / 2 && y % S == S / 2) {
-        centers.push_back(Vec2(x, y));
-        averageClusterColor(imIn, Vec2(x, y));
+        clusterCenters.push_back(Vec2(x, y));
+        clusterColors.push_back(averageClusterColor(imIn, Vec2(x, y)));
         // TODO: Adjust centers in 3x3 neighborhood
       }
     }
   }
-  return centers;
 }
 
 float colorDistance(ImageBase& imIn, Vec2 pixel, Vec2 center) {
@@ -82,46 +80,41 @@ float colorDistance(ImageBase& imIn, Vec2 pixel, Vec2 center) {
   return dr * dr + dg * dg + db * db;
 }
 
-void createClusters(ImageBase& imIn, std::vector<Vec2> centers, int S) {
+void createClusters(ImageBase& imIn, int S) {
   for (int y = 0; y < imIn.getHeight(); ++y) {
     for (int x = 0; x < imIn.getWidth(); ++x) {
       Vec2 pixel(x, y);
       float bestDistance = -1;
       int bestCenterIdx;
-      Vec3 bestCenterColor;
-      for (int i = 0; i < centers.size(); i++) {
-        Vec2 center = centers[i];
-        float distanceXY = (center - pixel).length();
-
-        if (distanceXY > 2*S) {
+      for (int i = 0; i < clusterCenters.size(); i++) {
+        Vec2 center = clusterCenters[i];
+        if (std::abs(center[0] - x) > S || std::abs(center[1] - y) > S) {
           continue;
         } 
-
+        
+        float distanceXY = (center - pixel).length();
         float distanceC = colorDistance(imIn, pixel, center);
         float distance = distanceC + (COMPACTNESS / S) * distanceXY;
 
         if (distance < bestDistance || bestDistance == -1) {
           bestDistance = distance;
           bestCenterIdx = i;
-          
-          int a = center[0];
-          int b = center[1];
-          int centerId = ((a + b) * (a + b + 1)) / 2 + b;
-          bestCenterColor = clusterColors[centerId];
         }
       }
       // This part must be changed
       // ___________________ 
-      imIn[x * 3][y * 3 + 0] = bestCenterColor[0];
-      imIn[x * 3][y * 3 + 1] = bestCenterColor[1];
-      imIn[x * 3][y * 3 + 2] = bestCenterColor[2];
+      Vec3 clusterColor = clusterColors[bestCenterIdx];
+      imIn[x * 3][y * 3 + 0] = clusterColor[0];
+      imIn[x * 3][y * 3 + 1] = clusterColor[1];
+      imIn[x * 3][y * 3 + 2] = clusterColor[2];
       // ___________________
     }
   }
 }
 
-void drawCenters(ImageBase& imIn, std::vector<Vec2> centers) {
-  for (Vec2 center : centers) {
+void drawCenters(ImageBase& imIn) {
+  for (int i = 0; i < clusterCenters.size(); i++) {
+    Vec2 center = clusterCenters[i];
     int x = center[0];
     int y = center[1];
 
@@ -132,6 +125,9 @@ void drawCenters(ImageBase& imIn, std::vector<Vec2> centers) {
         imIn[j * 3][i * 3 + 2] = 128;
       }
     }
+    imIn[x * 3][y * 3 + 0] = clusterColors[i][0];
+    imIn[x * 3][y * 3 + 1] = clusterColors[i][1];
+    imIn[x * 3][y * 3 + 2] = clusterColors[i][2];
   }
 }
 
@@ -157,7 +153,7 @@ int main(int argc, char** argv) {
   imIn.load(nameImgIn);
 
   int S = std::sqrt((imIn.getHeight() * imIn.getWidth()) / nSuperpixels); // Distance between centers
-  std::vector<Vec2> centers = findInitialCenters(imIn, S);
+  findInitialCenters(imIn, S);
 
   ImageBase imOut(imIn.getWidth(), imIn.getHeight(), imIn.getColor());
   for (int y = 0; y < imIn.getHeight(); ++y) {
@@ -167,8 +163,8 @@ int main(int argc, char** argv) {
       imOut[x * 3][y * 3 + 2] = imIn[x * 3][y * 3 + 2];
     }
   }
-  createClusters(imOut, centers, S);
-  drawCenters(imOut, centers);
+  createClusters(imOut, S);
+  drawCenters(imOut);
   imOut.save(nameImgOur);
 
   return 0;
